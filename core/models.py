79502890c608da426cd997d1575bcf38eceec744
Jason O'Dell <jasoneodell@gmail.com>
2025-06-03 15:27:48 -0400

    Adding is featured flag for service categories

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class ServiceCategory(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    is_featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
