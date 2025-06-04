7f2bfa1182c80038ff25356642255d89708aa45c
Jason O'Dell <jasoneodell@gmail.com>
2025-06-04 18:32:15 -0400

    Updating styles and content of request forms

--- core/forms.py
+++ core/forms.py
@@ -18,6 +18,7 @@ class ServiceRequestForm(forms.ModelForm):
         fields = ["requested_date", "hours_requested", "description"]
         widgets = {
             "requested_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+            "hours_requested": forms.NumberInput(attrs={"min": 0.25, "step": 0.25}),
             "description": forms.Textarea(attrs={"rows": 4}),
         }
 
@@ -25,8 +26,11 @@ class ServiceRequestForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ["bio", "phone_number", "email", "address"]
+        fields = ["bio", "phone_number", "email", "address", "image"]
         widgets = {
             "bio": forms.Textarea(attrs={"rows": 4}),
+            "phone_number": forms.TextInput(attrs={"type": "tel"}),
+            "email": forms.EmailInput(attrs={"type": "email"}),
             "address": forms.Textarea(attrs={"rows": 3}),
+            "image": forms.FileInput(attrs={"type": "file"}),
         }
