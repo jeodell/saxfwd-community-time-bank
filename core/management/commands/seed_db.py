a6290c0ca1825fec85cdb1d209472478f1639820
Jason O'Dell <jasoneodell@gmail.com>
2025-06-08 15:31:03 -0400

    Alphabetizing added categories

--- core/management/commands/seed_db.py
+++ core/management/commands/seed_db.py
@@ -3,24 +3,24 @@ from django.core.management.base import BaseCommand
 from core.models import ServiceCategory
 
 CATEGORIES = [
+    {"name": "Arts and Crafts", "is_featured": True},
+    {"name": "Education", "is_featured": False},
+    {"name": "Elder Care", "is_featured": False},
     {"name": "Entertainment", "is_featured": False},
-    {"name": "Wellness", "is_featured": False},
-    {"name": "Other", "is_featured": False},
-    {"name": "Legal", "is_featured": False},
+    {"name": "Financial", "is_featured": True},
+    {"name": "Fitness", "is_featured": False},
+    {"name": "Food", "is_featured": True},
     {"name": "General Labor", "is_featured": True},
+    {"name": "Healthcare", "is_featured": False},
+    {"name": "Home", "is_featured": False},
+    {"name": "Landscaping", "is_featured": True},
+    {"name": "Legal", "is_featured": False},
+    {"name": "Other", "is_featured": False},
     {"name": "Pet", "is_featured": False},
     {"name": "Repair", "is_featured": False},
-    {"name": "Transportation", "is_featured": False},
-    {"name": "Elder Care", "is_featured": False},
-    {"name": "Fitness", "is_featured": False},
-    {"name": "Financial", "is_featured": True},
-    {"name": "Healthcare", "is_featured": False},
-    {"name": "Education", "is_featured": False},
-    {"name": "Landscaping", "is_featured": True},
-    {"name": "Arts and Crafts", "is_featured": True},
-    {"name": "Home", "is_featured": False},
-    {"name": "Food", "is_featured": True},
     {"name": "Tech", "is_featured": True},
+    {"name": "Transportation", "is_featured": False},
+    {"name": "Wellness", "is_featured": False},
 ]
 
 DESCRIPTION = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a."
