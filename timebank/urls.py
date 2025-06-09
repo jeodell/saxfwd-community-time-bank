8fbf9d35b86a36b762d825e511b4201672d12d5d
Jason O'Dell <jasoneodell@gmail.com>
2025-06-09 12:12:12 -0400

    Changing admin route

--- timebank/urls.py
+++ timebank/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("timebank-admin/", admin.site.urls),
     path("", include("core.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
