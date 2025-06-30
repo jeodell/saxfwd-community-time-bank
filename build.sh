64733743039618f1f5bd700d3a86e2e0f50273e2
Jason O'Dell <jasoneodell@gmail.com>
2025-06-29 22:10:07 -0400

    Updating build command

---
+++ build.sh
@@ -0,0 +1,6 @@
+set -o errexit
+pip install -r requirements.txt
+cd theme/static_src && npm install && npm run build && cd ../..
+python manage.py tailwind build
+python manage.py collectstatic --noinput
+python manage.py migrate
