b3004fd79b13b627dc5becb62250487479915230
Jason O'Dell <jasoneodell@gmail.com>
2025-06-29 21:04:04 -0400

    Trying to fix static and media urls again

--- timebank/settings.py
+++ timebank/settings.py
@@ -113,13 +113,13 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Media files
-MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- timebank/urls.py
+++ timebank/urls.py
@@ -1,19 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
-urlpatterns = (
-    [
-        path("timebank-admin/", admin.site.urls),
-        path("accounts/", include("django.contrib.auth.urls")),
-        path("", include("core.urls.base")),
-        path("services/", include("core.urls.service")),
-        path("requests/", include("core.urls.request")),
-        path("users/", include("core.urls.user")),
-        path("ledger/", include("core.urls.ledger")),
-    ]
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    + staticfiles_urlpatterns()
-)
+urlpatterns = [
+    path("timebank-admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", include("core.urls.base")),
+    path("services/", include("core.urls.service")),
+    path("requests/", include("core.urls.request")),
+    path("users/", include("core.urls.user")),
+    path("ledger/", include("core.urls.ledger")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
