a22796c65938cb035a756dc2a49f0b33ca3256a9
Jason O'Dell <jasoneodell@gmail.com>
2025-06-29 21:00:30 -0400

    Attempting to fix static file issue

--- timebank/urls.py
+++ timebank/urls.py
@@ -1,17 +1,19 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
-urlpatterns = [
-    path("timebank-admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("", include("core.urls.base")),
-    path("services/", include("core.urls.service")),
-    path("requests/", include("core.urls.request")),
-    path("users/", include("core.urls.user")),
-    path("ledger/", include("core.urls.ledger")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = (
+    [
+        path("timebank-admin/", admin.site.urls),
+        path("accounts/", include("django.contrib.auth.urls")),
+        path("", include("core.urls.base")),
+        path("services/", include("core.urls.service")),
+        path("requests/", include("core.urls.request")),
+        path("users/", include("core.urls.user")),
+        path("ledger/", include("core.urls.ledger")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + staticfiles_urlpatterns()
+)
