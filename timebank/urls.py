bfdd0d5d60bf6f12a8bfc82eb14ced93990100c7
Jason O'Dell <jasoneodell@gmail.com>
2025-06-29 21:16:52 -0400

    Try try again

--- timebank/urls.py
+++ timebank/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
 urlpatterns = (
@@ -13,6 +14,6 @@ urlpatterns = (
         path("users/", include("core.urls.user")),
         path("ledger/", include("core.urls.ledger")),
     ]
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + staticfiles_urlpatterns()
 )
