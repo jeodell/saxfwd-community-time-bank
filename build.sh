07e4c94995e5fa621c9fec7c05c464f040318e3f
Jason O'Dell <jasoneodell@gmail.com>
2025-07-06 11:47:24 -0400

    Adding create_admin command

--- build.sh
+++ build.sh
@@ -4,3 +4,11 @@ cd theme/static_src && npm install && npm run build && cd ../..
 python manage.py tailwind build
 python manage.py collectstatic_s3
 python manage.py migrate
+python manage.py create_admin \
+  --email admin@example.com \
+  --password securepassword123 \
+  --first-name Admin \
+  --last-name User \
+  --phone "555-1234" \
+  --address "123 Main St" \
+  --bio "System administrator"
