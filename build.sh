d406a54769d931b9263911f14a670d12daa1cf95
Jason O'Dell <jasoneodell@gmail.com>
2025-07-06 17:30:44 -0400

    Removing superuser creation from build

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@ cd theme/static_src && npm install && npm run build && cd ../..
 python manage.py tailwind build
 python manage.py collectstatic_s3
 python manage.py migrate
-DJANGO_SUPERUSER_PASSWORD=superuser python manage.py createsuperuser --noinput --email superuser@example.com --first_name Admin --last_name User
