e45fe5aefbd46bf30d2967ff88bd3e8873673758
Jason O'Dell <jasoneodell@gmail.com>
2025-07-27 13:54:57 -0400

    Adding paths for translations

--- timebank/urls.py
+++ timebank/urls.py
@@ -1,9 +1,16 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+# Non-internationalized URLs
 urlpatterns = [
+    path("i18n/", include("django.conf.urls.i18n")),
+]
+
+# Internationalized URLs
+urlpatterns += i18n_patterns(
     path("timebank-admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("core.urls.base")),
@@ -12,7 +19,8 @@ urlpatterns = [
     path("users/", include("core.urls.user")),
     path("community/", include("core.urls.community")),
     path("admin/", include("core.urls.admin")),
-]
+    prefix_default_language=False,
+)
 
 # Serve static and media files in development only
 if settings.DEBUG and not settings.USE_S3:
